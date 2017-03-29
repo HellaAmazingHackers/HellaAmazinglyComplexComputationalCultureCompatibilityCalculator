83f870bdc7d13daca9773aae8cd608818cebd922
Andrew Alonis <andrew.alonis@gmail.com>
2017-03-29 10:44:22 -0700

    Create new navigation tab and component for tradeoff analytics

---
+++ client/src/components/Tradeoff.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as s from '../serverCalls.js';
+import * as globalData from '../sampledata'
+
+
+class Tradeoff extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        Tradeoff
+      </div>
+    )
+  }
+}
+
+export default Tradeoff
